03929b3c440c5df21b4b76196a8d48b56982523d
Johannes Gilbert <johannes.gilbert@sap.com>
2023-04-23 19:52:42 +0200

    disable hooks triggering by cron

--- routes/index.js
+++ routes/index.js
@@ -1316,8 +1316,8 @@ var job = new CronJob(
     }, reason => {
       console.error(`Job ended with error. See below.`)
       console.error(reason);
-    })
-    .then(triggerHooks('cron'));
+    });
+    //.then(triggerHooks('cron'));
 	},
 	null,
 	true,
